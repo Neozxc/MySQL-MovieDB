f3cc847492b2ce43c7f9d9a0fe93ed66c1f8ef14
Lapinas <mrlapinas99@gmail.com>
2022-02-10 10:42:43 +0000

    aSync await connection

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,2 +1,24 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
+
+let sequalize = new Sequelize(process.env.MYSQL_URI);
+
+// sequalize.authenticate().then(() => {
+//     console.log("Connected to the Database");
+// }).catch((error) => {
+//     console.log(error);
+// });
+
+
+// aSync await
+let runSequalize = async () => {
+    try {
+        await sequalize.authenticate();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+runSequalize();
+
+// module.exports = sequalize;
